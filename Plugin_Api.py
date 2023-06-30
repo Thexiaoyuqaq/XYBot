b94388e57e809744321fa11d3d60d7acf7845218
Thexiaoyu <50231954+Thexiaoyuqaq@users.noreply.github.com>
2023-07-01 02:08:25 +0800

    对Plugin_API重新排版

--- Plugin_Api.py
+++ Plugin_Api.py
@@ -4,7 +4,12 @@ import os
 
 
 def load_plugins():
-    
+    """
+    加载插件目录中的插件文件并创建插件对象
+
+    Returns:
+        list: 插件对象列表
+    """
     curr_time = datetime.datetime.now()
     time_str = datetime.datetime.strftime(curr_time, '%H:%M:%S')
     event_Time = "[" + time_str + "]"
@@ -33,8 +38,16 @@ def load_plugins():
 
     return plugins
 
+
 async def Plugins_Group_Message(message, plugins):
-    # 处理消息的逻辑
+    """
+    处理群消息的逻辑
+
+    Args:
+        message (str): 接收到的消息
+        plugins (list): 插件对象列表
+
+    """
     for plugin_name, plugin in plugins:
         if hasattr(plugin, "GroupMessage") and callable(getattr(plugin, "GroupMessage")):
             try:
@@ -49,24 +62,42 @@ async def Plugins_Group_Message(message, plugins):
             time_str = datetime.datetime.strftime(curr_time, '%H:%M:%S')
             event_Time = "[" + time_str + "]"
             #print(event_Time + f"[警告][插件][跳过][消息][{plugin_name}] 插件缺少 'GroupMessage' 方法，跳过执行。")
+
+
 async def Plugins_Request(message, plugins):
-    # 处理消息的逻辑
+    """
+    处理请求事件的逻辑
+
+    Args:
+        message (str): 请求消息
+        plugins (list): 插件对象列表
+
+    """
     for plugin_name, plugin in plugins:
-        if hasattr(plugin, "Requst") and callable(getattr(plugin, "Requst")):
+        if hasattr(plugin, "Request") and callable(getattr(plugin, "Request")):
             try:
-                await plugin.Requst(message)
+                await plugin.Request(message)
             except Exception as e:
                 curr_time = datetime.datetime.now()
                 time_str = datetime.datetime.strftime(curr_time, '%H:%M:%S')
                 event_Time = "[" + time_str + "]"
-                print(event_Time + f"[错误][插件][执行][事件][进群][{plugin_name}] Error: {str(e)}")
+                print(event_Time + f"[错误][插件][执行][事件][请求][{plugin_name}] Error: {str(e)}")
         else:
             curr_time = datetime.datetime.now()
             time_str = datetime.datetime.strftime(curr_time, '%H:%M:%S')
             event_Time = "[" + time_str + "]"
-            #print(event_Time + f"[警告][插件][跳过][事件][进群][{plugin_name}] 插件缺少 'Requst' 方法，跳过执行。")
+            #print(event_Time + f"[警告][插件][跳过][事件][请求][{plugin_name}] 插件缺少 'Request' 方法，跳过执行。")
+
+
 async def Plugins_Notice_join(message, plugins):
-    # 处理消息的逻辑
+    """
+    处理进群事件的逻辑
+
+    Args:
+        message (str): 进群消息
+        plugins (list): 插件对象列表
+
+    """
     for plugin_name, plugin in plugins:
         if hasattr(plugin, "Notice_join") and callable(getattr(plugin, "Notice_join")):
             try:
@@ -81,8 +112,17 @@ async def Plugins_Notice_join(message, plugins):
             time_str = datetime.datetime.strftime(curr_time, '%H:%M:%S')
             event_Time = "[" + time_str + "]"
             #print(event_Time + f"[警告][插件][跳过][事件][进群][{plugin_name}] 插件缺少 'Notice_join' 方法，跳过执行。")
+
+
 async def Plugins_Notice_leave(message, plugins):
-    # 处理消息的逻辑
+    """
+    处理退群事件的逻辑
+
+    Args:
+        message (str): 退群消息
+        plugins (list): 插件对象列表
+
+    """
     for plugin_name, plugin in plugins:
         if hasattr(plugin, "Notice_leave") and callable(getattr(plugin, "Notice_leave")):
             try:
@@ -97,4 +137,3 @@ async def Plugins_Notice_leave(message, plugins):
             time_str = datetime.datetime.strftime(curr_time, '%H:%M:%S')
             event_Time = "[" + time_str + "]"
             #print(event_Time + f"[警告][插件][跳过][事件][退群][{plugin_name}] 插件缺少 'Notice_leave' 方法，跳过执行。")
-
