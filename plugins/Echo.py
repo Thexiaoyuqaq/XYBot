d1ec99a6a03af8d54e1600bf6c3eef433531ccf5
Endermite <105129407+EndermiteX@users.noreply.github.com>
2023-07-04 04:54:29 +0800

    添加Echo实例插件

---
+++ plugins/Echo.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+# @FileName :Echo.py
+# @Time     : 4:50
+# @Author   :Endermite
+
+from Api import send_Groupmessage
+from Api import send_FriendMessage
+
+
+class Plugin:
+    @staticmethod
+    async def GroupMessage(event_original):
+        group_id = event_original["group_id"]
+        message = event_original["message"]
+        await send_Groupmessage(Group_ID=group_id, Message_ID=0, Message=message, awa=False)
+
+    @staticmethod
+    async def FriendMessage(event_original):
+        user_id = event_original["user_id"]
+        message = event_original["message"]
+        await send_FriendMessage(user_id=user_id, message=message)
