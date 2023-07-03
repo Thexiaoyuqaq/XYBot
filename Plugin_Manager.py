2be49bfa4d2cc9b182800ac16192995000578226
Endermite <105129407+EndermiteX@users.noreply.github.com>
2023-07-04 04:28:36 +0800

    添加Debug日志判断

--- Plugin_Manager.py
+++ Plugin_Manager.py
@@ -29,10 +29,7 @@ def load_plugins():
 
     logger.info(message="正在获取插件列表...", flag=FILE_NAME)
     pluginList = os.listdir("plugins")
-
-    if get_config('main', 'Debug'):
-        logger.debug(message="插件列表：" + str(pluginList), flag=FILE_NAME)
-
+    logger.debug(message="插件列表：" + str(pluginList), flag=FILE_NAME)
     logger.info(message="正在加载插件", flag=FILE_NAME)
     for plugin in pluginList:
         if plugin.endswith(".py"):
